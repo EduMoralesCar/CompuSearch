0264e3c8ef11d3938534d51900a4a417670a08ec
Edu Morales <edumoralescarlos@gmail.com>
2025-08-19 18:37:38 -0500

    Avance 1 - Estructura del Proyecto

---
+++ src/main/java/com/universidad/compuSearch/CompuSearchApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.compuSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompuSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompuSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universidad/compuSearch/CompuSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.compuSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompuSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
